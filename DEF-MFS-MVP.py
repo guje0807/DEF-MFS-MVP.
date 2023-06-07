96a9b5e1db717e92f1db779edb30c79d60b5b579
Akash Guje <101294578+guje0807@users.noreply.github.com>
2023-06-07 10:18:26 -0400

    Data Storage Changes
    Created a new class named data_storage and member function to read the aws configuration from the JSON file and upload the file to the S3 bucket.

--- DEF-MFS-MVP.py
+++ DEF-MFS-MVP.py
@@ -4,11 +4,12 @@ Created on Wed May 31 08:19:16 2023
 
 @author: aakas
 """
-
 import pandas as pd
 import argparse
 import yfinance as yf
 
+a = __import__("DEF-MFS-MVP-Storage")
+ 
 
 #Class Definition for Specific Stock company
 class stock:
@@ -27,28 +28,47 @@ class stock:
         df.reset_index(inplace=True)
         print("Returning Stock Data")
         return df
+
+#Function to store data to S3.
+def store_data(fileName,ticker_symbol):
+    ds = a.dataStorage()
+    ds.read_config()
     
+    if(ds.upload_object(fileName,ticker_symbol)):
+        print("File Upload Sucessfull!!")
+    else:
+        print("File Upload Failed!!")
+ 
+    
+#Main Function
+def main():
+    print("Usage: DEF-MFS-MVP.py -t ticker_sysmbol -s start_date -e end_date") #Printing the Usage of the file 
+    
+    argParser = argparse.ArgumentParser() 
+    argParser.add_argument("-t", "--ticker_symbol", help="Ticker Symbol") # Adding Ticker Symbol Argument 
+    argParser.add_argument("-s", "--start_date", help="Start Date") # Adding Start Date Argument
+    argParser.add_argument("-e", "--end_date", help="End Date") # Adding End Date Argument
+    
+    args = argParser.parse_args() # Loading the Arguments to a variable
+    
+    print("Passed Argumnets are:", args) # Printing the Arguments 
+    
+    stock_data = stock(args.ticker_symbol, args.start_date, args.end_date) #Declaring the Object with ticker symbol,start date and end date.
+    
+    df = stock_data.download_stock_info() # Calling the Class member function
+    
+    fileName = f"{stock_data.ticker_symbol}.xlsx"
+    
+    df.to_excel(f"{stock_data.ticker_symbol}.xlsx") #Downloading the stock data to excel file
+    
+    #print(df.head(10)) # Printing the top 10 rows of the stock data.
+    
+    # Uploading data to S3
+    store_data(fileName,stock_data.ticker_symbol)
 
 #Start
 
-print("Usage: DEF-MFS-MVP.py -t ticker_sysmbol -s start_date -e end_date") #Printing the Usage of the file 
-
-argParser = argparse.ArgumentParser() 
-argParser.add_argument("-t", "--ticker_symbol", help="Ticker Symbol") # Adding Ticker Symbol Argument 
-argParser.add_argument("-s", "--start_date", help="Start Date") # Adding Start Date Argument
-argParser.add_argument("-e", "--end_date", help="End Date") # Adding End Date Argument
-
-args = argParser.parse_args() # Loading the Arguments to a variable
-
-print("Passed Argumnets are:", args) # Printing the Arguments 
-
-stock_data = stock(args.ticker_symbol, args.start_date, args.end_date) #Declaring the Object with ticker symbol,start date and end date.
-
-df = stock_data.download_stock_info() # Calling the Class member function
-
-df.to_excel(f"{stock_data.ticker_symbol}.xlsx") #Downloading the stock data to excel file
-
-print(df.head(10)) # Printing the top 10 rows of the stock data. 
+main()
 
 #End
     
